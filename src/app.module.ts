aec6a103439e6e7a6cd43dc3d7ca9bbf426d5412
NautroKazenaki <kalliak2014@yandex.ru>
2024-05-06 13:18:51 +0300

    service, controller, module, interface, dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsController } from './catsPage/cats.controller';
+import { CatsService } from './catsPage/cats.service';
+import { CatsModule } from './catsPage/cats.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CatsModule],
+  controllers: [AppController, CatsController],
+  providers: [AppService, CatsService],
 })
 export class AppModule {}
