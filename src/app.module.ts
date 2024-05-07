9388515a4d25d24688eaf91dde6ef736a515923c
NautroKazenaki <kalliak2014@yandex.ru>
2024-05-07 09:15:50 +0300

    middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,24 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './catsPage/cats.controller';
 import { CatsService } from './catsPage/cats.service';
 import { CatsModule } from './catsPage/cats.module';
+import { LoggerMiddleware } from './catsPage/common/middleware/logger.middleware';
 
 @Module({
   imports: [CatsModule],
   controllers: [AppController, CatsController],
   providers: [AppService, CatsService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .exclude(
+        { path: 'cats', method: RequestMethod.POST },
+        'cats/(.*)'
+      )
+      .forRoutes({ path: 'cats', method: RequestMethod.GET });
+  }
+}
